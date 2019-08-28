36be95d18fd66aef9866695f72e8a98ee55ca7a9
Mihaitron <popamihai18@yahooc.om>
2019-08-28 15:18:26 +0300

    Logs wether player posseses a tank or not

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* Player = GetControlledTank();
+
+	if (Player)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s controlled"), *Player->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No tank controlled"), *Player->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
