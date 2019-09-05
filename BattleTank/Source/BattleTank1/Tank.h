101d4e21336f5514a7a692822f62f4de6f962228
Popa Mihai <popamihai18@yahoo.com>
2019-09-05 14:39:17 +0300

    Fly-by-wire forward and backward movement added

--- BattleTank/Source/BattleTank1/Tank.h
+++ BattleTank/Source/BattleTank1/Tank.h
@@ -37,8 +37,6 @@ protected:
 	virtual void BeginPlay() override;
 
 	UTankAimingComponent* TankAimingComponent = nullptr;
-	UPROPERTY(BlueprintReadOnly)
-	UTankMovementComponent* TankMovementComponent = nullptr;
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category = Firing)
--- BattleTank/Source/BattleTank1/TankTrack.cpp
+++ BattleTank/Source/BattleTank1/TankTrack.cpp
@@ -6,6 +6,9 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
+	// auto WorldTime = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throttle);
+
 	// TODO Clamp throttle value so player can't over-drive
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
